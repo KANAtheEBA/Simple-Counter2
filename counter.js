e47b7208277cae52b2bc9798877088872f7f5412
km-mainPC <kmatsumoto.fw@gmail.com>
2024-07-15 16:24:56 +0900

    adjusted and finishesd

--- counter.js
+++ counter.js
@@ -11,7 +11,7 @@
         }
     }
 
-    for(let index = 0; index < document.getElementsByClassName("js-button").length; index++){
-        document.getElementsByClassName("js-button") [index].addEventListener("click", (e) => clickHandler(e))
+    for(let index = 0; index < document.getElementsByClassName("button").length; index++){
+        document.getElementsByClassName("button") [index].addEventListener("click", (e) => clickHandler(e))
     }
 })();
